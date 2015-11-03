1d9cca6865078a431e7227974f8b71dabf4d0800
tusizi <529340225@qq.com>
2015-11-03 16:00:03 +0800

    SearchAticleServlet

--- src/cn/com/leadfar/cms/backend/model/Article.java
+++ src/cn/com/leadfar/cms/backend/model/Article.java
@@ -1,9 +1,16 @@
 package cn.com.leadfar.cms.backend.model;
 
+import java.util.Date;
+
 /**
  * Created by tusizi on 2015/10/22.
  */
 public class Article {
+    private  int id;
+    private String source;
+    private Date createtime;
+    private Date deploytime;
+    private Date updatetime;
     private  String title;
     private  String context;
 
@@ -15,11 +22,51 @@ public class Article {
         this.context = context;
     }
 
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getDeploytime() {
+        return deploytime;
+    }
+
+    public void setDeploytime(Date deploytime) {
+        this.deploytime = deploytime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
     }
 }
--- src/cn/com/leadfar/cms/backend/view/SearchArticlesServlet.java
+++ src/cn/com/leadfar/cms/backend/view/SearchArticlesServlet.java
@@ -1,6 +1,7 @@
 package cn.com.leadfar.cms.backend.view;
 
 import cn.com.leadfar.cms.backend.model.Article;
+import cn.com.leadfar.cms.utils.DBUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by tusizi on 2015/10/22.
@@ -22,12 +29,31 @@ public class SearchArticlesServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //查询文章列表
-        List articles = new ArrayList();
-        for (int i = 0; i < 10; i++) {
-            Article a = new Article();
-            a.setTitle("这是文章标题");
-            a.setContext("这是文章内容" + i);
-            articles.add(a);
+        List articles = new ArrayList();//定义一个文章列表
+        String sql = "select * from t_article";//定义一个sql语句
+        Connection conn = DBUtil.getConn();//连接数据库
+        PreparedStatement pstmt = null;//预处理语句
+        ResultSet rs = null;//定义一个结果集
+        try {
+            pstmt = conn.prepareStatement(sql);//将SQL放入到预处理语句里
+            rs = pstmt.executeQuery();//预处理语句执行查询，将结果放入到结果集rs里
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD HH-MM-SS");
+            while(rs.next()){
+                Article article = new Article();
+                article.setTitle(rs.getString("title"));
+                article.setContext(rs.getString("content"));
+                article.setSource(rs.getString("source"));
+                article.setCreatetime(rs.getTimestamp("createtime"));
+                article.setUpdatetime(rs.getTimestamp("updatetime"));
+                article.setDeploytime(rs.getTimestamp("deploytime"));
+                articles.add(article);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtil.close(rs);
+            DBUtil.close(pstmt);
+            DBUtil.close(conn);
         }
         request.setAttribute("articles", articles);
         System.out.println(articles);
