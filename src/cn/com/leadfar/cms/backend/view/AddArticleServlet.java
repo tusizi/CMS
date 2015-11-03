c82218b7ed76e7637671f0780a46070c51b30d8d
tusizi <529340225@qq.com>
2015-11-03 16:51:38 +0800

    add article

---
+++ src/cn/com/leadfar/cms/backend/view/AddArticleServlet.java
@@ -0,0 +1,19 @@
+package cn.com.leadfar.cms.backend.view;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by tusizi on 2015/11/3.
+ */
+@WebServlet(name = "AddArticleServlet")
+public class AddArticleServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
