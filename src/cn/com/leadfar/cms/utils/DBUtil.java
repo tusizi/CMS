d2aeccc605007c7b34bb543d95d4a977718e01e8
gitprimos <primos@126.com>
2015-11-28 18:05:04 +0800

    database

--- src/cn/com/leadfar/cms/utils/DBUtil.java
+++ src/cn/com/leadfar/cms/utils/DBUtil.java
@@ -8,12 +8,35 @@ import java.sql.*;
  * Created by tusizi on 2015/10/20.
  */
 public class DBUtil {
-    public static Connection getConn() {
-        JdbcConnectionPool cp = JdbcConnectionPool.create("jdbc:h2:~/cms", "sa", null);
 
+//    public static Connection getConn() {
+//        JdbcConnectionPool cp = JdbcConnectionPool.create("jdbc:h2:~/cms", "sa", null);
+//
+//        Connection conn = null;
+//        try {
+//            conn=  cp.getConnection();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return conn;
+//    }
+
+    public static Connection getConn() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");//注册一个驱动
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //2.创建数据库连接
+        //url:数据库连接协议
+
+        String url = "jdbc:mysql://localhost/cms";
         Connection conn = null;
         try {
-            conn=  cp.getConnection();
+            conn = DriverManager.getConnection(url, "root", "root");
+            conn.setAutoCommit(false);//表示关闭事务自动提交
         } catch (SQLException e) {
             e.printStackTrace();
         }
